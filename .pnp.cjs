9a834d7db20e3a43298ca603edb1552e84ca6b89
Maryam <maryam.esmaeilzadeh@erasys.de>
2025-01-25 16:43:45 +0100

    fix: pnp

--- .pnp.cjs
+++ .pnp.cjs
@@ -35,7 +35,7 @@ const RAW_RUNTIME_STATE =
           ["@biomejs/biome", "npm:1.9.4"],\
           ["@tailwindcss/postcss", "npm:4.0.0"],\
           ["@types/gh-pages", "npm:6.1.0"],\
-          ["@types/node", "npm:22.10.7"],\
+          ["@types/node", "npm:22.10.10"],\
           ["@types/react", "npm:19.0.8"],\
           ["@types/react-dom", "virtual:46043f39e53e8a0f301391269908e5a3adc5dfd577f05f536609b6e88166b12e07cfca935ac3a4156939ea6c5a4026515779d8bec5a80e7fe9bd9c43c30caa7e#npm:19.0.3"],\
           ["@types/webpack-env", "npm:1.18.6"],\
@@ -3106,10 +3106,10 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "HARD"\
       }],\
-      ["npm:22.10.7", {\
-        "packageLocation": "./.yarn/cache/@types-node-npm-22.10.7-d71170a7fa-c941b4689d.zip/node_modules/@types/node/",\
+      ["npm:22.10.10", {\
+        "packageLocation": "./.yarn/cache/@types-node-npm-22.10.10-31182c993e-3425772d45.zip/node_modules/@types/node/",\
         "packageDependencies": [\
-          ["@types/node", "npm:22.10.7"],\
+          ["@types/node", "npm:22.10.10"],\
           ["undici-types", "npm:6.20.0"]\
         ],\
         "linkType": "HARD"\
@@ -10528,7 +10528,7 @@ const RAW_RUNTIME_STATE =
           ["@biomejs/biome", "npm:1.9.4"],\
           ["@tailwindcss/postcss", "npm:4.0.0"],\
           ["@types/gh-pages", "npm:6.1.0"],\
-          ["@types/node", "npm:22.10.7"],\
+          ["@types/node", "npm:22.10.10"],\
           ["@types/react", "npm:19.0.8"],\
           ["@types/react-dom", "virtual:46043f39e53e8a0f301391269908e5a3adc5dfd577f05f536609b6e88166b12e07cfca935ac3a4156939ea6c5a4026515779d8bec5a80e7fe9bd9c43c30caa7e#npm:19.0.3"],\
           ["@types/webpack-env", "npm:1.18.6"],\
